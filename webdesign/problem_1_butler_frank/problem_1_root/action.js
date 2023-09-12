1a3f867a1a1d1b6b6ca16bcd2a32423a808699f0
bfrank12 <dbfrank03@gmail.com>
2023-09-12 15:18:55 -0400

    commit

--- webdesign/problem_1_butler_frank/problem_1_root/action.js
+++ webdesign/problem_1_butler_frank/problem_1_root/action.js
@@ -8,3 +8,25 @@ image.addEventListener("click", function() {
         image.requestFullscreen();
     }
 });
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+  slides[slideIndex-1].style.display = "block";
+}
